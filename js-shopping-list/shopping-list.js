d7d0916257f12e8475931cbbbf5fab3a5f67f9d0
Cedric <75742716+cedorikku@users.noreply.github.com>
2024-06-28 22:00:55 +0800

    Implement add button function that adds item

--- js-shopping-list/shopping-list.js
+++ js-shopping-list/shopping-list.js
@@ -1,3 +1,22 @@
 const itemList = document.querySelector('ul');
 const input = document.getElementById('input');
 const addButton = document.querySelector('button');
+
+addButton.addEventListener('click', () => {
+  const newItem = input.value;
+  input.value = '';
+
+  const newListItem = document.createElement('li');
+  const listItemValue = document.createElement('span');
+  const deleteButton = document.createElement('button');
+
+  newListItem.appendChild(listItemValue);
+  newListItem.appendChild(deleteButton);
+
+  listItemValue.textContent = toString(newItem);
+  deleteButton.textContent = "Delete";
+
+  itemList.appendChild(newListItem);
+
+  input.focus();
+});
