3af7518230d89d4e6a37c1c46c1d8a92a2c4a4c4
Cedric <75742716+cedorikku@users.noreply.github.com>
2024-06-28 21:03:53 +0800

    Track basic DOM nodes study

--- js-nodes/nodes.js
+++ js-nodes/nodes.js
@@ -1,3 +1,18 @@
-const first = document.querySelector('.first-section');
-const second = document.querySelector('.second-section');
+const firstSection = document.querySelector('.first-section');
+const secondSection = document.querySelector('.second-section');
 
+const myPara = document.createElement('p');
+const anotherPara = document.createElement('p');
+const myText = document.createTextNode(`Text to be added later`);
+const anotherText = `More text to be added later`;
+
+function myLoadFunction() {
+
+    firstSection.appendChild(myPara);
+    myPara.appendChild(myText);
+
+    firstSection.appendChild(anotherPara);
+    anotherPara.textContent = anotherText;
+
+    secondSection.remove();
+}
