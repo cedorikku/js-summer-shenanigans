5ca8968a1af5b9ec9bb54e03692b6e7b8676a3f4
Cedric <75742716+cedorikku@users.noreply.github.com>
2024-07-01 11:03:19 +0800

    Update for latest exercise

--- js-button-forms/four-buttons-zipped/script.js
+++ js-button-forms/four-buttons-zipped/script.js
@@ -3,28 +3,45 @@ const answer = document.getElementById('answer');
 const first = document.getElementById('first');
 const second = document.getElementById('second');
 
-let x, y;
+function myFunction(operator) {
+    let x = first.value;
+    let y = second.value;
+    let ans;
 
-function add() {
-    x = first.value;
-    y = second.value;
-    answer.textContent = `${x} + ${y} = ` + parseInt(x + y);
+    switch (operator) {
+        case '+':
+            add();
+            break;
+        case '-':
+            subtract(x,y);
+            break;
+        case '*':
+            ans = multiply(x,y);
+            answer.textContent = `${x} ${operator} ${y} = ${ans}`;
+            break;
+        case '/':
+            ans = divide(x,y);
+            answer.textContent = `${x} ${operator} ${y} = ${ans}`;
+            break;
+    }
 }
 
-function subtract() {
-    x = first.value;
-    y = second.value;
+function add() {
+    let x = parseInt(first.value);
+    let y = parseInt(second.value);
+    answer.textContent = `${x} + ${y} = ${(x + y)}`;
+}
+
+function subtract(x,y) {
     answer.textContent = `${x} - ${y} = ` + (x - y);
 }
 
-function multiply() {
-    x = first.value;
-    y = second.value;
-    answer.textContent = `${x} x ${y} = ` + (x * y);
+function multiply(x,y) {
+    // answer.textContent = `${x} x ${y} = ` + (x * y);
+    return x * y;
 }
 
-function divide() {
-    x = first.value;
-    y = second.value;
-    answer.textContent = `${x} / ${y} = ` + (x / y);
+function divide(x,y) {
+    // answer.textContent = `${x} / ${y} = ` + (x / y);
+    return x / y;
 }
